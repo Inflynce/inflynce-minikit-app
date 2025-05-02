7cbefcbd2651ce034db2c93909dda1b9ae763457
Jie Liu <jieliu@Jies-MacBook-Pro.local>
2025-05-02 14:13:09 +0300

    Refactor daily task

--- src/components/dailyTask/VisitInflynceTask.tsx
+++ src/components/dailyTask/VisitInflynceTask.tsx
@@ -1,29 +1,12 @@
 import {
   Typography,
-  Box,
-  Button,
   ListItem,
   ListItemText,
-  CardMedia,
-  Card,
-  CardContent,
-  CardActions,
-  IconButton,
-  Avatar,
-  ListItemAvatar,
   Chip,
   Stack,
-  CircularProgress,
 } from '@mui/material';
 import React from 'react';
 import { User_Tasks } from '@/__generated__/graphql';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
-import AssignmentTurnedInTwoToneIcon from '@mui/icons-material/AssignmentTurnedInTwoTone';
 import TodayIcon from '@mui/icons-material/Today';
 import { LoadingButton } from '@mui/lab';
 
@@ -49,7 +32,7 @@ const VisitInflynceTask: React.FC<VisitInflynceTaskProps> = ({
 
   return (
     <ListItem
-      sx={{ bgcolor: 'rgba(255, 255, 255, 0.05)', borderRadius: 1 }}
+      sx={{ bgcolor: 'rgba(255, 255, 255, 0.05)', borderRadius: 1, pr: 12 }}
       secondaryAction={
         <LoadingButton
           loading={isPending}
@@ -63,24 +46,14 @@ const VisitInflynceTask: React.FC<VisitInflynceTaskProps> = ({
         </LoadingButton>
       }
     >
-      <ListItemAvatar>
-        <Avatar>
-          <AssignmentTurnedInTwoToneIcon />
-        </Avatar>
-      </ListItemAvatar>
       <ListItemText>
+        
+        <Stack direction="row" spacing={1} sx={{ mb: 1 }}>
         <Typography variant="body1" gutterBottom>
           {taskData.title}
         </Typography>
-        <Stack direction="row" spacing={1} sx={{ mb: 1 }}>
           <Chip
             label={`+${taskData.rewardIp} IP`}
-            size="small"
-            sx={{ fontSize: '10px', color: 'text.secondary' }}
-          />
-          <Chip
-            icon={<TodayIcon sx={{ fontSize: '12px !important', color: 'text.secondary' }} />}
-            label={`${format(task.date, 'MMM dd')}`}
             size="small"
             sx={{ fontSize: '10px', color: 'text.secondary' }}
           />
